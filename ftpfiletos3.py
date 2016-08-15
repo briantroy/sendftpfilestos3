f4bb52db87280cb509a9b17f2420ce965400f86e
Brian Roy <brian@briantroy.com>
2016-08-15 12:30:35 -0700

    Basic working version...

--- ftpfiletos3.py
+++ ftpfiletos3.py
@@ -1,6 +1,15 @@
 from tail import follow
 
 
-base_dir = "/home/securityspy/security-images/alarm-images"
-ftp_log_file = "/var/log/vsftpd.log"
+def main():
+    base_dir = "/home/securityspy/security-images/alarm-images"
+    ftp_log_file = "/var/log/vsftpd.log"
+
+    with open(ftp_log_file, 'rt') as following:
+        following.seek(-64, 2)
+        try:
+            for line in follow(following):
+                sys.stdout.write(line)
+        except KeyboardInterrupt:
+            pass
 
