e4285dae56ee2df8ecf4c9252b3133af46c48e0b
Brian Roy <brian@briantroy.com>
2016-08-15 15:22:54 -0700

    More parsing of the file name for processing to S3

--- ftpfiletos3.py
+++ ftpfiletos3.py
@@ -4,7 +4,6 @@ from tail import follow
 
 def main():
     import sys
-    base_dir = "/home/securityspy/security-images/alarm-images"
     ftp_log_file = "/var/log/vsftpd.log"
 
     with open(ftp_log_file, 'rt') as following:
@@ -17,9 +16,18 @@ def main():
             pass
 
 def parse_upload_file_line(line):
-    import sys 
+    import sys
+    import boto3
+    base_dir = "/home/securityspy/security-images/alarm-images"
     line_parts = line.split(",")
     sys.stdout.write("File for upload is: " + line_parts[1] + " with file size: " + line_parts[2] + "\n")
+    s3 = boto3.resource('s3')
+    # Parse the file name to get the sub-folder and object name
+    path_end = line_parts[1].replace(base_dir, "")
+    path_parts = path_end.split('/')
+    sys.stdout.write("File of type: " + path_parts[2] + " for camera " + path_parts[0] + " with file name " + path_parts[3] + "\n")
+
+    # s3.Object('security-alarms', foo).put(Body=open('/tmp/hello.txt', 'rb'))
     sys.exit(0)
 
 if __name__ == "__main__":
