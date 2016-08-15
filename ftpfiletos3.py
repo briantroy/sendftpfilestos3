e127504c851b188ecd24f78e0361290bd3570ccd
Brian Roy <brian@briantroy.com>
2016-08-15 15:10:25 -0700

    Tweaks to get the right file name from the line and multi-threading

--- ftpfiletos3.py
+++ ftpfiletos3.py
@@ -1,8 +1,9 @@
-import sys, threading
+import threading
 from tail import follow
 
 
 def main():
+    import sys
     base_dir = "/home/securityspy/security-images/alarm-images"
     ftp_log_file = "/var/log/vsftpd.log"
 
@@ -11,14 +12,15 @@ def main():
         try:
             for line in follow(following):
                 if "OK UPLOAD" in line:
-                    sys.stdout.write(line)
                     t = threading.Thread(target=parse_upload_file_line, args=(line, )).start()
         except KeyboardInterrupt:
             pass
 
 def parse_upload_file_line(line):
+    import sys 
     line_parts = line.split(",")
-    sys.stdout.write(line_parts[2])
+    sys.stdout.write("File for upload is: " + line_parts[1] + " with file size: " + line_parts[2] + "\n")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
