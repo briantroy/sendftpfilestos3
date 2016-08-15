5e082786aa0204f5c82c3dd11c87d8c52a179fa0
Brian Roy <brian@briantroy.com>
2016-08-15 15:39:22 -0700

    More parsing of the file name for processing to S3

--- ftpfiletos3.py
+++ ftpfiletos3.py
@@ -18,6 +18,10 @@ def main():
 def parse_upload_file_line(line):
     import sys
     import boto3
+    from datetime import date, timedelta
+    start_date = date.today()
+    date_string = start_date.strftime('%Y') + "-" + start_date.strftime("%m") + "-" + start_date.strftime("%d")
+    hour_string = "Hour-" + start_date.hour
     base_dir = "/home/securityspy/security-images/alarm-images"
     line_parts = line.split(",")
     sys.stdout.write("File for upload is: " + line_parts[1] + " with file size: " + line_parts[2] + "\n")
@@ -26,8 +30,8 @@ def parse_upload_file_line(line):
     path_end = line_parts[1].replace(base_dir, "")
     path_parts = path_end.split('/')
     sys.stdout.write("File of type: " + path_parts[3] + " for camera " + path_parts[1] + " with file name " + path_parts[4] + "\n")
-
-    # s3.Object('security-alarms', foo).put(Body=open('/tmp/hello.txt', 'rb'))
+    s3_object = "/" + path_parts[1] + "/" date_string + "/" + hour_string + "/" + path_parts[1] + "/" + path_parts[4]
+    s3.Object('security-alarms', s3_object).put(Body=open(line_parts[1], 'rb'))
     sys.exit(0)
 
 if __name__ == "__main__":
