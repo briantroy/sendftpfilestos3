a6ec6381af9e478c4cab6c533f8e153cdb844384
Brian Roy <brian@briantroy.com>
2016-08-19 15:34:14 -0700

    Fixing subprocess call

--- ftpfiletos3.py
+++ ftpfiletos3.py
@@ -84,7 +84,7 @@ def transcodetomp4(file_in):
     convert_command = '/usr/bin/avconv -i "{}" -f mp4 -vcodec copy -acodec libfaac -b:a 112k -ac 2 -y "{}"'.format(file_in, file_out)
 
     try:
-        subprocess.check_call(convert_command)
+        subprocess.check_call(convert_command, shell=True)
     except subprocess.CalledProcessError:
         return file_in
 
