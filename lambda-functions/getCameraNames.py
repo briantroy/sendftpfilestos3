f5f85c23823ebac46a0cde1331538685dabd5e63
Brian Roy <brian@briantroy.com>
2025-01-10 15:54:39 -0700

    filtering camera list to remove old cameras without videos in last 120 days

--- lambda-functions/getCameraNames.py
+++ lambda-functions/getCameraNames.py
@@ -2,6 +2,11 @@
 def lambda_handler(event, context):
     """ Lambda Hander """
     import boto3
+    from boto3.dynamodb.conditions import Key
+    import time
+
+    dyndb = boto3.resource('dynamodb')
+    days_since_last_video = 120
 
     s3_client = boto3.client('s3')
     items = s3_client.list_objects(Bucket='security-alarms', Prefix='patrolcams/', Delimiter='/')
@@ -11,7 +16,27 @@ def lambda_handler(event, context):
         camera = obj.get('Prefix')
         camera = camera.replace('patrolcams/', '')
         camera = camera.replace('/', '')
-        out_list.append(camera)
+        
+        # See if we have videos in the last 6 months for this camera
+        vid_table = dyndb.Table('security_video_timeline')
+        table_name = "security_alarm_videos"
+        select_attribs = 'ALL_ATTRIBUTES'
+        index_forward = False
+        key_condition = Key('camera_name').eq(camera)
+        
+        response = vid_table.query(
+            TableName=table_name,
+            Select=select_attribs,
+            ScanIndexForward=index_forward,
+            KeyConditionExpression=key_condition,
+            Limit=1,
+        )
+        for item in response['Items']:
+            if item['event_ts']/1000 > (time.time() - ((days_since_last_video)*24*60*60)):
+                out_list.append(camera)
+            # end If
+            break
         # end for
+    # end for
 
     return out_list
