d7a2859f30ce072e5d3dfea9cd90ba49aa17d7b9
Brian Roy <brian@briantroy.com>
2025-01-11 15:14:33 -0700

    updating the lambda to get camera names

--- lambda-functions/getCameraNames.py
+++ lambda-functions/getCameraNames.py
@@ -2,41 +2,39 @@
 def lambda_handler(event, context):
     """ Lambda Hander """
     import boto3
-    from boto3.dynamodb.conditions import Key
     import time
+    import json
 
-    dyndb = boto3.resource('dynamodb')
     days_since_last_video = 120
 
-    s3_client = boto3.client('s3')
-    items = s3_client.list_objects(Bucket='security-alarms', Prefix='patrolcams/', Delimiter='/')
-    print("Request for camera list.")
+    camera_list = get_s3_camera_metadata()
     out_list = []
-    for obj in items.get('CommonPrefixes', []):
-        camera = obj.get('Prefix')
-        camera = camera.replace('patrolcams/', '')
-        camera = camera.replace('/', '')
-        
-        # See if we have videos in the last 6 months for this camera
-        vid_table = dyndb.Table('security_video_timeline')
-        table_name = "security_alarm_videos"
-        select_attribs = 'ALL_ATTRIBUTES'
-        index_forward = False
-        key_condition = Key('camera_name').eq(camera)
-        
-        response = vid_table.query(
-            TableName=table_name,
-            Select=select_attribs,
-            ScanIndexForward=index_forward,
-            KeyConditionExpression=key_condition,
-            Limit=1,
-        )
-        for item in response['Items']:
-            if item['event_ts']/1000 > (time.time() - ((days_since_last_video)*24*60*60)):
-                out_list.append(camera)
-            # end If
-            break
-        # end for
+
+    for camera in camera_list['camera-last-video']:
+        if float(camera_list['camera-last-video'][camera]) > (time.time() - ((days_since_last_video)*24*60*60)):
+            out_list.append(camera)
+        # end If
     # end for
 
     return out_list
+
+def get_s3_camera_metadata():
+    import boto3
+    import json
+    bucket_name = "security-alarms-metadata"
+    metadata_file = "camera-info.json"
+    s3_resource = boto3.resource('s3')
+
+    content_object = s3_resource.Object(bucket_name, metadata_file)
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
+
+def put_s3_camera_metadata(camera_info):
+    import boto3
+    import json
+    bucket_name = "security-alarms-metadata"
+    metadata_file = "camera-info.json"
+    s3_resource = boto3.resource('s3')
+
+    s3_resource.Object(bucket_name, metadata_file).put(Body=json.dumps(camera_info))
